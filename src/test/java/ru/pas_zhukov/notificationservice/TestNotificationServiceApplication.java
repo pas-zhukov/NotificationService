2fd19f0b50c54c60c728ca9260e29899df1cc7bb
pas-zhukov <pas-zhukov@yandex.ru>
2024-11-30 16:46:23 +0300

    initial

---
+++ src/test/java/ru/pas_zhukov/notificationservice/TestNotificationServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.pas_zhukov.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(NotificationServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
